58bab409492807f0e804ba814b5ae4cd733731a4
Jahirul Islam <jahirulislamjantu002@gmail.com>
2024-12-29 18:49:27 +0600

    python learning

---
+++ index.py
@@ -0,0 +1,17 @@
+# print("Hello World")
+
+#  Variables in python is like just to say the name of the variable.
+
+# name = "Jahir"
+# print(name)
+
+# Python has a special prompt system called "input" it will take a value from the user and returns 
+myName = input("Name: ")
+# myName variable will store a value from user, and print function will concat the name with the given string.
+# This is a older string concat method used in python
+# print("Hello " + myName) 
+# This is a new version of string concat method in python, this is like template literal in JavaScript
+print(f"Hello, {myName}")
+
+
+
