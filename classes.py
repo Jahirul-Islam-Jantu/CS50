6bab62b7a43cb26d7678ee11047f9e05d9d062c3
Jahirul Islam <jahirulislamjantu002@gmail.com>
2024-12-30 00:45:32 +0600

    OOP in Python

---
+++ classes.py
@@ -0,0 +1,39 @@
+# Class based programming is called OOP 
+# class Point():
+#     def __init__(self, input1, input2):
+#         self.x = input1
+#         self.y = input2
+
+# p = Point(10, 20)
+# print(p.x)
+# print(p.y)
+# print(f"The value{p.x} and {p.y} is the points of a class, when we multiply {p.x * p.y} it will return ")
+
+# This is the basic structure of a class in python. and we are calling the class with two parameters and multiply them.
+# __init__ is called itself every times when value changes
+
+
+#  Let's create a airline ticket booking system
+
+class Flight():
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.passengers = []
+
+    def add_passengers(self, name):
+        if not self.open_seats():
+            return False
+        self.passengers.append(name)
+        return True
+    def open_seats(self):
+        return self.capacity - len(self.passengers)        
+    
+flight = Flight(3)
+
+peoples = ["Jahir", "Sahana", "Tawhid", "Samayrah", "Anamul", "Rana", "Keya"]
+
+for people in peoples:
+    if flight.add_passengers(people):
+        print(f"Added {people} in Passenger list for the flight")
+    else:
+        print(f"{people} not added because of no seats available")
--- squares.py
+++ squares.py
@@ -1,8 +1,24 @@
-from functions import square
+import functions
+# This is how we import a file in python. We can import a file by using the "import" keyword and the file name.
+for i in range(50):
+    print(f"The square of {i} is {functions.square(i)}")
+
+# To run this loop by calling the whole function by importing the file, we have to use the file name and the function name with a dot in between.
+
+
+
+
+
+
+# from functions import square
 # This is how we import a function from another file in python.
 #  We can import a function from another file by using the "from" keyword and the file name and function name.
 
-for i in range(100):
-    print(f"The square of {i} is {square(i)}")
+
+
+# for i in range(100):
+#     print(f"The square of {i} is {square(i)}")
 
 # This loop has no function to print the square of the numbers from 1 to 99. So we are importing the function from the functions.py file and calling the function in the loop to print the square of the numbers from 1 to 99.
+
+
