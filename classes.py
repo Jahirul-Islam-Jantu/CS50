a951d7f18144d07a868a1af819c36c609d038ab0
Jahirul Islam <jahirulislamjantu002@gmail.com>
2024-12-30 10:35:12 +0600

    student oop

--- classes.py
+++ classes.py
@@ -15,25 +15,54 @@
 
 #  Let's create a airline ticket booking system
 
-class Flight():
+#
+# class Flight():
+#     def __init__(self, capacity):
+#         self.capacity = capacity
+#         self.passengers = []
+#
+#     def add_passengers(self, name):
+#         if not self.open_seats():
+#             return False
+#         self.passengers.append(name)
+#         return True
+#
+#     def open_seats(self):
+#         return self.capacity - len(self.passengers)
+#
+#
+# flight = Flight(3)
+#
+# peoples = ["Jahir", "Sahana", "Tawhid", "Samayrah", "Anamul", "Rana", "Keya"]
+#
+# for people in peoples:
+#     if flight.add_passengers(people):
+#         print(f"Added {people} in Passenger list for the flight")
+#     else:
+#         print(f"{people} not added because of no seats available")
+
+
+class ClassRoom():
     def __init__(self, capacity):
         self.capacity = capacity
-        self.passengers = []
+        self.students = []
 
-    def add_passengers(self, name):
-        if not self.open_seats():
+    def add_student(self, name):
+        if not self.open_capacity():
             return False
-        self.passengers.append(name)
+        self.students.append(name)
         return True
-    def open_seats(self):
-        return self.capacity - len(self.passengers)        
-    
-flight = Flight(3)
 
-peoples = ["Jahir", "Sahana", "Tawhid", "Samayrah", "Anamul", "Rana", "Keya"]
+    def open_capacity(self):
+        return self.capacity - len(self.students)
 
-for people in peoples:
-    if flight.add_passengers(people):
-        print(f"Added {people} in Passenger list for the flight")
+
+classroom = ClassRoom(3)
+
+studentsList = ["Jahir","Jantu", "Keya", "Sahana"]
+
+for student in studentsList:
+    if classroom.add_student(student):
+        print(f"Student added Successfully, Name: {student}")
     else:
-        print(f"{people} not added because of no seats available")
+        print(f"No seat for {student}")
