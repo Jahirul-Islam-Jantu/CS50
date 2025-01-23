3a84fde69c2b2ca5310e9f135194c035574f3737
Jahirul Islam <jahirulislamjantu002@gmail.com>
2025-01-23 23:43:56 +0600

    todo edit

--- 60Days-Python/TodoApp-for-terminal/todo.py
+++ 60Days-Python/TodoApp-for-terminal/todo.py
@@ -2,14 +2,26 @@
 
 todos = []
 while True:
-    user_input = input("Please Type add , show or exit: ")
+    user_input = input("Please Type add , show, edit or exit: ")
+    user_input = user_input.strip()
     match user_input:
         case "add":
             todo = input("Add Item: ")
             todos.append(todo)
         case "show":
-            print(todos)
+            for item in todos:
+                item = item.title()
+                print(item)
+        case "edit":
+            number = int(input("Enter a Number for Edit Todo: "))
+            number = number - 1
+            new_todo = input("Enter new Todo: ")
+            todos[number] = new_todo
         case "exit":
             break
+        case _:
+            print("Hey, You entered Wrong keyword")
+
+
 
 print(f"Bye Bye Dear. Your Todos are Here! {todos}")
